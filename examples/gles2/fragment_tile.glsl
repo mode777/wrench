6a5397001bd2d95475255f6f7869058840b915aa
Alexander Klingenbeck <mode7@gmx.net>
2021-01-07 16:46:00 +0100

    shader experiments

--- examples/gles2/fragment_tile.glsl
+++ examples/gles2/fragment_tile.glsl
@@ -2,13 +2,16 @@ uniform sampler2D texture;
 
 varying mediump vec2 texcoord;
 uniform mediump vec2 texSize;
+uniform mediump int sw;
 
 void main(void) {
-  mediump vec4 tile = texture2D(texture, texcoord / texSize);
-  mediump vec2 oneTile = (texSize / 16.0);
-  
-  mediump vec2 coordinates = tile.xy * 255.0;
-  mediump vec2 offset = fract(texcoord);
+  if(sw == 0){
+    mediump vec4 tile = texture2D(texture, texcoord / texSize);
+    mediump vec2 oneTile = (texSize / 16.0);
+    
+    mediump vec2 coordinates = tile.xy * 255.0;
+    mediump vec2 offset = fract(texcoord);
 
-  gl_FragColor = texture2D(texture, (coordinates + offset) / oneTile);
+    gl_FragColor = texture2D(texture, (coordinates + offset) / oneTile);
+  }
 }
--- examples/gles2/vertex_tile.glsl
+++ examples/gles2/vertex_tile.glsl
@@ -5,36 +5,40 @@ attribute vec2 trans;
 uniform vec2 size;
 uniform mediump vec2 texSize;
 uniform float prio;
+uniform mediump int sw;
 
 varying vec2 texcoord;
 
 void main(void) {
-  vec2 uv = coordUv.zw - (size / 64.0);
+  if(sw == 0){
+    vec2 uv = coordUv.zw - (size / 64.0);
 
-  float r = -(scaleRot.z / 10430.0);
-  float s = sin(r);
-  float c = cos(r);
-  float sx = 4096.0 / scaleRot.x;
-  float sy = 4096.0 / scaleRot.y;
-  float sprio = scaleRot.w;
-  //float mult = step(prio, sprio) * step(sprio, prio);
-  //sx *= mult;
-  //sy *= mult;
+    float r = -(scaleRot.z / 10430.0);
+    float s = sin(r);
+    float c = cos(r);
+    float sx = 4096.0 / scaleRot.x;
+    float sy = 4096.0 / scaleRot.y;
+    float sprio = scaleRot.w;
+    float mult = step(prio, sprio) * step(sprio, prio);
+    sx *= mult;
+    sy *= mult;
 
-  float m0 = sx * c;
-  float m1 = sx * s;
+    float m0 = sx * c;
+    float m1 = sx * s;
 
-  float m3 = sy * -s;
-  float m4 = sy * c;
+    float m3 = sy * -s;
+    float m4 = sy * c;
 
-  float m6 = -(trans.x-(size.x/2.0)) / 32.0;
-  float m7 = -(trans.y-(size.y/2.0)) / 32.0;
+    float m6 = -(trans.x-(size.x/2.0)) / 32.0;
+    float m7 = -(trans.y-(size.y/2.0)) / 32.0;
 
-  mat3 transformation = mat3(m0, m1, 0.0, m3, m4, 0.0, m6, m7, 1.0);
-  texcoord = (transformation * vec3(uv, 1.0)).xy;
+    mat3 transformation = mat3(m0, m1, 0.0, m3, m4, 0.0, m6, m7, 1.0);
+    texcoord = (transformation * vec3(uv, 1.0)).xy;
 
-  //transformation = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0);
+    //transformation = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0);
 
-  vec2 xy = (coordUv.xy / (size / 2.0) - 1.0) * vec2(1.0, -1.0);
-  gl_Position = vec4(xy, 0.0, 1.0);
+    vec2 xy = (coordUv.xy / (size / 2.0) - 1.0) * vec2(1.0, -1.0);
+    gl_Position = vec4(xy, 0.0, 1.0);
+  }
+
 }
