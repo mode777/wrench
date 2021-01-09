b140b3248cf47dbdbd02abd1329aa3667d05acc6
Alexander Klingenbeck <mode7@gmx.net>
2021-01-09 15:20:42 +0100

    for RPI

--- examples/gles2/fragment_tile.glsl
+++ examples/gles2/fragment_tile.glsl
@@ -9,18 +9,16 @@ uniform highp vec2 tilesize;
 void main(void) {
   highp vec4 tile = texture2D(texture, texcoord / texSize);
   tile *= 255.0;
-  // highp float prioFlag = tile.z;
+  highp float prioFlag = tile.z;
   highp float uPrio = mod(prio, 2.0);
   highp float tPrio = sign(tile.z);
   highp float mult = step(tPrio, uPrio) * step(uPrio, tPrio);
-  tile *= mult;
+  //tile *= mult;
 
   highp vec2 oneTile = (texSize / tilesize);
   
   highp vec2 offset = fract(texcoord);
-
-  highp vec4 pixel = texture2D(texture, (tile.xy + offset) / oneTile);
-
-
+  highp vec4 pixel = texture2D(texture, (tile.xy + offset) / oneTile);  
+  pixel.a *= mult;
   gl_FragColor = pixel;
 }
