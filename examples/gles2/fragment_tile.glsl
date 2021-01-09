e3be7ecdf86f3a6564081afd11c50ea1f084d9e2
Alexander Klingenbeck <mode7@gmx.net>
2021-01-09 12:55:58 +0100

    for RPI

--- examples/gles2/fragment_tile.glsl
+++ examples/gles2/fragment_tile.glsl
@@ -9,10 +9,10 @@ uniform mediump vec2 tilesize;
 void main(void) {
   mediump vec4 tile = texture2D(texture, texcoord / texSize);
   tile *= 255.0;
-  mediump float prioFlag = tile.z;
-  mediump float prioSet = mod(prio, 2.0);
-  mediump float mult = step(prioFlag, prioSet) * step(prioSet, prioFlag);
-  tile *= mult;
+  // mediump float prioFlag = tile.z;
+  // mediump float prioSet = mod(prio, 2.0);
+  // mediump float mult = step(prioFlag, prioSet) * step(prioSet, prioFlag);
+  // tile *= mult;
 
   mediump vec2 oneTile = (texSize / tilesize);
   
