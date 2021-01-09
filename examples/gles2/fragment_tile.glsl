2bcd55788f29fa2d61a29f8b1c668541bb323d69
Alexander Klingenbeck <mode7@gmx.net>
2021-01-09 16:21:10 +0100

    for RPI

--- examples/gles2/fragment_tile.glsl
+++ examples/gles2/fragment_tile.glsl
@@ -10,10 +10,10 @@ void main(void) {
   lowp vec4 tile = texture2D(texture, texcoord / texSize);
   tile *= 255.0;
   lowp float prioFlag = tile.z;
-  lowp float uPrio = sign(mod(prio, 2.0));
+  lowp float uPrio = mod(prio, 2.0);
   lowp float tPrio = sign(tile.z);
-  lowp float mult = step(tPrio, uPrio) * step(uPrio, tPrio);
-  //tile *= mult;
+  lowp float mult = step(0.2, abs(uPrio - tPrio));
+  tile *= mult;
 
   lowp vec2 oneTile = (texSize / tilesize);
   
