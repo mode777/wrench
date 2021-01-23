f13bb9ac3d582de7acf1ca50db9fe9658a7fdfd2
Alexander Klingenbeck <mode7@gmx.net>
2021-01-23 15:56:00 +0100

    rpi

--- wren_modules/super16/fragment_tile.glsl
+++ wren_modules/super16/fragment_tile.glsl
@@ -15,22 +15,17 @@ void main(void) {
   // water wobble
   //inp.x += mod(gl_FragCoord.y, 2.0) * -sin(gl_FragCoord.y / 50.0 + time) + mod(gl_FragCoord.y+1.0, 2.0) * sin(gl_FragCoord.y / 35.0 + time);
   //inp.y += cos(gl_FragCoord.y / 35.0 + time);
+
   //inp.x *= gl_FragCoord.y / 10.0;
 
   lowp vec4 tile = texture2D(map, (floor(inp)+0.5) / mapSize);
-  //lowp vec4 tile = texture2D(map, (floor(inp)+0.5) * 0.0078125);
-
   tile *= 255.0;
-  lowp float mul = step(0.1, tile.x + tile.y) * (1.0 - floor(mod(tile.z + prio + 0.1, 2.0)));
-  tile *= mul;
+  tile *= 1.0 - floor(mod(tile.z + prio + 0.1, 2.0));
 
-  //lowp vec2 oneTile = (texSize / tilesize);
-  lowp float oneTile = 0.015625;
+  lowp vec2 oneTile = (texSize / tilesize);
 
   lowp vec2 offset = ((floor(fract(inp) * tilesize / pixelation) + 0.5) / tilesize * pixelation);
-  //lowp vec2 offset = ((floor(fract(inp) * tilesize) + 0.5) * 0.0625);
-  lowp vec2 uv = (tile.xy + offset) * oneTile;
+  lowp vec2 uv = (tile.xy + offset) / oneTile;
 
   gl_FragColor = texture2D(texture, uv);
-  gl_FragColor.a *= mul;
 }
