62c844e2373d8b670f8205905d72dc65aff4f36b
Alexander Klingenbeck <mode7@gmx.net>
2021-01-23 15:25:58 +0100

    rpi

--- wren_modules/super16/fragment_tile.glsl
+++ wren_modules/super16/fragment_tile.glsl
@@ -15,11 +15,10 @@ void main(void) {
   // water wobble
   //inp.x += mod(gl_FragCoord.y, 2.0) * -sin(gl_FragCoord.y / 50.0 + time) + mod(gl_FragCoord.y+1.0, 2.0) * sin(gl_FragCoord.y / 35.0 + time);
   //inp.y += cos(gl_FragCoord.y / 35.0 + time);
-
   //inp.x *= gl_FragCoord.y / 10.0;
 
-  lowp vec4 tile = texture2D(map, (floor(inp)+0.5) / mapSize);
-  //if((tile.x + tile.y) == 0.0) discard;
+  //lowp vec4 tile = texture2D(map, (floor(inp)+0.5) / mapSize);
+  lowp vec4 tile = texture2D(map, (floor(inp)+0.5) * 0.0078125);
 
   tile *= 255.0;
   tile *= 1.0 - floor(mod(tile.z + prio + 0.1, 2.0));
